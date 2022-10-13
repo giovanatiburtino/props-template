c63d48fdc42180d62feafbf1de98db6bd1df6757
giova <giovanaftiburtino@gmail.com>
2022-10-13 13:30:51 -0300

    Exercício realizado

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const nome = 'Giovana'
+  const nome2 = 'Ana'
+  function apresentaGaragem(){
+    alert(`Boas vindas à garagem da ${nome}`)
+
+  }
   return (
     <div className="App">
-      <Garagem />
+      <Garagem nome={nome} mensagemApresentacao={apresentaGaragem}/>
+      <Garagem nome={nome2} mensagemApresentacao={apresentaGaragem}/>
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,18 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
   return (
     <div>
-      <h1>Garagem de Indio</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem de {props.nome}</h1>
+      <button onClick={props.mensagemApresentacao}>Mensagem</button>
+      <Carro adicionadoPor={props.nome} cor={"Vermelho"} ano={2022} flex={"true"}/>
+      <Carro adicionadoPor={props.nome} cor={"Verde"} ano={2020} flex={"false"}/>
+      <Carro adicionadoPor={props.nome} cor={"Amarelo"} ano={2009} flex={"false"}/>
+      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} flex={"true"}/>
+      <Carro adicionadoPor={props.nome} cor={"Branco"} ano={2021} flex={"true"}/>
+      <Carro adicionadoPor={props.nome} cor={"Cinza"} ano={2015} flex={"false"}/>
+      <Carro adicionadoPor={props.nome} cor={"Azul"} ano={2017} flex={"true"}/>
+      <Carro adicionadoPor={props.nome} cor={"Roxo"} ano={2012} flex={"false"}/>
     </div>
   );
 }
